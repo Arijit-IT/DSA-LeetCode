a971919d12207b32cdb2620f2547f21c90fb6bba
ARIJIT NANDI <167028611+Arijit-IT@users.noreply.github.com>
2026-07-28 17:55:58 +0530

    Time: 0 ms (100%), Space: 43.7 MB (25.51%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,41 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int start=0;
-        int end=nums.length-1;        
-        mergesort(nums,start,end);
-    }
-    public static void mergesort(int []nums,int start,int end){
-        if(start>=end){
-            return;
-        }
-        int mid=start+(end-start)/2;
-
-        mergesort(nums,start,mid);
-        mergesort(nums,mid+1,end);
-        merge(nums,start,mid,end);
-
-    }
-    public static void merge(int []arr,int start,int mid, int end){
-        int []temp=new int[end-start+1];
-        int i=start;
-        int j=mid+1;
-        int k=0;
-        while(i<=mid && j<=end){
-            if(arr[i]<=arr[j]){
-                temp[k++]=arr[i++]; 
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count0++;
             }
-            else{
-                temp[k++]=arr[j++];
+            else if(nums[i]==1){
+                count1++;
+
             }
+            else count2++;
         }
-        while(i<=mid){
-            temp[k++]=arr[i++];
-        }
-        while(j<=end){
-            temp[k++]=arr[j++];
-        }
-        for (int p = 0; p < temp.length; p++) {
-            arr[start + p] = temp[p];
+        for(int i=0;i<nums.length;i++){
+            if(count0>0){
+                nums[i]=0;
+                count0--;
+            }
+            else if(count1>0){
+                nums[i]=1;
+                count1--;
+            }
+            else nums[i]=2;
         }
     }
     
